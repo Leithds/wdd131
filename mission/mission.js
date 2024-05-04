aeb456a01b819fb78c3e502b1f3282cdb139d9e3
Leithds <154087236+Leithds@users.noreply.github.com>
2024-05-03 20:11:50 -0600

    alright! I got it working!

--- mission/mission.js
+++ mission/mission.js
@@ -1,17 +1,14 @@
-const themeSelector = document.getElementsByClassName('light-select');
-
-function changeTheme() {
-    const currentTheme = themeSelector.value;
+document.addEventListener('DOMContentLoaded', function() {
+    const themeSelector = document.getElementById('light-select');
     const bodyElement = document.body;
-    const logo = document.querySelector('footer img');
 
-    if (currentTheme === 'Dark') {
-        bodyElement.classList.add('dark');
-        logo.src = 'image/byui-logo_white.png';
-    } else {
-        bodyElement.classList.remove('dark');
-        logo.src = 'image/byui-logo_blue.webp';
-    }
-}
-
-themeSelector.addEventListener('change', changeTheme);
+    themeSelector.addEventListener('change', function() {
+        const currentTheme = themeSelector.value;
+        
+        if (currentTheme === 'Dark') {
+            bodyElement.classList.add('dark');
+        } else {
+            bodyElement.classList.remove('dark');
+        }
+    });
+});
