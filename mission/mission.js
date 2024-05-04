352ebd7f9600363b76dc1252e6839e17c3aaf47f
Leithds <154087236+Leithds@users.noreply.github.com>
2024-05-03 19:54:52 -0600

    maybe i fixed it?

--- mission/mission.js
+++ mission/mission.js
@@ -1,17 +1,17 @@
-const themeSelector = document.getElementById('light-select');
+const themeSelector = document.getElementsByClassName('light-select');
 
 function changeTheme() {
     const currentTheme = themeSelector.value;
     const bodyElement = document.body;
-    const logo = document.querySelector('BYU-I dark logo');
+    const logo = document.querySelector('footer img');
 
     if (currentTheme === 'Dark') {
         bodyElement.classList.add('dark');
-        logo.src = 'image/byui-logo_white.webp';
+        logo.src = 'image/byui-logo_white.png';
     } else {
         bodyElement.classList.remove('dark');
         logo.src = 'image/byui-logo_blue.webp';
     }
 }
-nt
+
 themeSelector.addEventListener('change', changeTheme);
